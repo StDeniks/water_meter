563cb8c689fdbd430b48e1fd8afca2afe3031141
StDeniks <st.deniks@gmail.com>
2020-07-03 20:20:35 +0300

    Initial commit

---
+++ water_meter.ino
@@ -0,0 +1,13 @@
+// Select pins for watermeters. It pins have to have interraptions. There are 2 and 3 in Arduino NANO
+#define CNTR_H_PIN 2
+#define CNTR_C_PIN 3
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
